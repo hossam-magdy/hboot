27e65b04a05ae1f208634c82c74716c7474d72a4
Hossam Magdy <hossam.magdy@outlook.com>
2020-04-25 15:44:00 +0200

    Initial GRUB2 working attempt
    Loading Windows 10 ISO worked only with memdisk
    Loading Ubuntu ISO worked!
    Win7PE not tested yet

--- boot/firadisk/AUTORUN.cmd
+++ boot/firadisk/AUTORUN.cmd
@@ -1,6 +1,7 @@
 @echo off
 ::THIS FILE IS STARTED BEFOR WINDOWS INSTALLATION FROM ISO STARTS
 ::cls
-START /min "" "%~d0\boot\_windows_product_keys_.txt"
+START /min "" notepad "%~dp0WindowsProductKeys"
 cmd /k %~dp0LOADISO.cmd
+START /wait "" notepad "%~dp0WindowsProductKeys"
 exit
--- boot/firadisk/ISONAME.cmd
+++ boot/firadisk/ISONAME.cmd
@@ -1,12 +1,27 @@
-SET MYISO=\win10_x64.iso
-                                                                                                                                            
-                                                                                                                                                                      
-                                                                                                                                                                      
-                                                                                                                                                                      
-                                                                                                                                                                      
-                                                                                                                                                                      
-                                                                                                                                                                      
-                                                                                                                                                                      
-                                                                                                                                                                      
-                                                                                                                                                                      
-                                                                                                                                 
+@echo off
+:: SET MYISO=\boot\ISOs\win10_x64.iso
+
+SET ENV_FILE=%~d0\boot\grub\grubenv
+::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: "\boot\grub\grubenv" is expected to include line:
+:: ISO_FILE=some/path/file.iso
+::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+FOR /F "tokens=*" %%i in (%ENV_FILE%) DO CALL :ProcessLine %%i
+:: SET MYISO=%ISO_FILE%
+GOTO End
+
+:ProcessLine
+SET line=%*
+IF "%line:~0,8%" == "ISO_FILE" CALL :VarFound %line%
+GOTO :eof
+
+:VarFound
+SET %*
+SET MYISO=%ISO_FILE:/=\%
+::IF "%MYISO:~0,1%" == "/" SET MYISO=%MYISO:~1%
+GOTO :eof
+
+:End
+::echo %MYISO%
+::pause
