0d078d67bd83c12dbee81ef4c9c51769a1db1ec7
Hossam Magdy <hossam.magdy@outlook.com>
2020-04-26 02:33:55 +0200

    Fix installer-ubuntu

--- boot/grub-2.04-installer/installer-ubuntu.sh
+++ boot/grub-2.04-installer/installer-ubuntu.sh
@@ -3,12 +3,14 @@
 # Ref: http://manpages.ubuntu.com/manpages/focal/man8/grub-install.8.html
 
 PLATFORM="i386-pc"
-BASEDIR=$(dirname `realpath "$0"`)
+BASEDIR=$(dirname $(realpath "$0"))
 DEVICE=$(df --output=source . | sed -n '2 p' | sed 's/.$//') # e.g: /dev/sdc
-BOOT_DIR=$(df --output=target . | sed -n '2 p')/boot # e.g: /mount/MyUsb/boot
+BOOT_DIR=$(df --output=target . | sed -n '2 p')/boot         # e.g: /mount/MyUsb/boot
 
 INSTALL_CMD="grub-install --boot-directory=$BOOT_DIR --directory=$BASEDIR/$PLATFORM $DEVICE"
 echo INSTALL_CMD=$INSTALL_CMD
-pause 'Confirm? Press [Enter] key to continue...'
+echo "Press [Enter] key to continue...";
+read _
 
 sh -c "$INSTALL_CMD" && cp "$BASEDIR/grub.cfg" "$BOOT_DIR/grub"
+
