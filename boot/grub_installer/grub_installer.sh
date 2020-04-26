3da08cf03c3b09482160b32e2689b89ecea4c4fd
Hossam Magdy <hossam.magdy@outlook.com>
2020-04-26 22:18:05 +0200

    Update grub_installer for ubuntu

--- boot/grub_installer/grub_installer.sh
+++ boot/grub_installer/grub_installer.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-TARGET_DEV=$(df --output=source . | sed -n '2 p' | sed 's/.$//') # e.g: /dev/sdc
-FULL_MBR="./grub_mbr"
+TARGET_DEV=$1
+CURRENT_DEV=$(df --output=source . | sed -n '2 p' | sed 's/.$//') # e.g: /dev/sdc
+MBR_FILE=$(realpath $(dirname "$0")/grub_mbr)
 LABEL="HNew"
-# export TARGET_DEV=/dev/sdb
-# export SIGNATURE_BK=./grub1Attempt6sig.mbr
 
+[ ! -f "$MBR_FILE" ] && echo "Exiting… MBR file not found: $MBR_FILE" && exit 1
 [ "$TARGET_DEV" == "/dev/sda" ] && echo 'Exiting… should not run on device "/dev/sda"' && exit 1
-echo "Target device: $TARGET_DEV … Press [Enter] to continue…"
+
+echo "CAUTION: Running this script on wrong device will lead to complete data loss"
+echo - Target device: $TARGET_DEV
+echo - MBR file: $MBR_FILE
+echo - Label: $LABEL
+echo "Press [Enter] to continue…"
 read _
 
 ################ WIPE/ERASE the device, create new PartitionTable
-# Completely erase first 10MB sectors
-sudo dd of=$TARGET_DEV if=/dev/zero bs=1M count=10
+# Erase the first 9216 bytes in device (the bytes where MBR, PT, GRUB will be located)
+sudo dd of=$TARGET_DEV if=/dev/zero bs=1 count=9216
 # Unmount
 sudo umount $TARGET_DEV?
 # Partition: 1 NTFS
@@ -29,9 +34,9 @@ sudo mkfs.ntfs -QFL $LABEL ${TARGET_DEV}1
 # 0 - 440 - 512 - 952 - 1024 - 9216
 #  MBR   P_T   GRU   P_T    GRU     (Goal)
 #  MBR   P_T   0000000000000000     (Initial)
-sudo dd of=$TARGET_DEV if=$FULL_MBR seek=0    skip=0    bs=1 count=440
-sudo dd of=$TARGET_DEV if=$FULL_MBR seek=512  skip=512  bs=1 count=440
-sudo dd of=$TARGET_DEV if=$FULL_MBR seek=1024 skip=1024 bs=1 count=8192
+sudo dd of=$TARGET_DEV if=$MBR_FILE seek=0 skip=0 bs=1 count=440
+sudo dd of=$TARGET_DEV if=$MBR_FILE seek=512 skip=512 bs=1 count=440
+sudo dd of=$TARGET_DEV if=$MBR_FILE seek=1024 skip=1024 bs=1 count=8192
 # Clone P_T for GRUB
 sudo dd if=$TARGET_DEV of=$TARGET_DEV skip=440 seek=952 bs=1 count=72
 
@@ -42,10 +47,11 @@ sudo dd if=$TARGET_DEV of=$TARGET_DEV skip=440 seek=952 bs=1 count=72
 # #X DUMP
 # sudo dd if=$TARGET_DEV of=./grub1Dump.mbr skip=0 ibs=1 count=9216
 # ################ METHOD 2: bk P_T, overwrite all, restore P_T
+# export SIGNATURE_BK=./grub1Attempt6sig.mbr
 # # EXTRACT SIGNATURE
 # sudo dd if=$TARGET_DEV of=$SIGNATURE_BK skip=440 ibs=1 count=72
 # # RESTORE FULL
-# sudo dd of=$TARGET_DEV if=$FULL_MBR seek=0 ibs=1 count=9216
+# sudo dd of=$TARGET_DEV if=$MBR_FILE seek=0 ibs=1 count=9216
 # # WRITE SIGNATURE
 # sudo dd of=$TARGET_DEV if=$SIGNATURE_BK seek=440 obs=1 count=72
 # sudo dd of=$TARGET_DEV if=$SIGNATURE_BK seek=952 obs=1 count=72
