13d384168d04d14eb52adefec001ef5b27823377
Hossam Magdy <hossam.magdy@outlook.com>
2020-04-29 22:11:23 +0200

    feat: implement iso listing in verify-iso.sh

--- iso/verify-iso.bat
+++ iso/verify-iso.bat
@@ -2,16 +2,17 @@
 ::SETLOCAL (disabled to enable gloibal vars in calling :Section_CopyFiles)
 SET _SCRIPT_DRIVE=%~d0
 SET _SCRIPT_PATH=%~dp0
-SET _ZipBootFilesInRoot=%_SCRIPT_DRIVE%/boot/.bootfiles-rootdir.zip
-SET _ContigEXETool=%_SCRIPT_DRIVE%/boot/.tool_SysinternalsContig.exe
+SET ROOT_DIR=%_SCRIPT_PATH%/..
+CD %ROOT_DIR%
+
+SET ISO_DIR=%ROOT_DIR%/iso
+SET _ContigEXETool=%ROOT_DIR%/tools/SysinternalsContig.exe
 SET FragISOLogFile=%TEMP%/.(Fragmented_ISO_Files).log
-SET menuISOFileList=%_SCRIPT_DRIVE%/boot/.menuISOFileList.lst
-%_SCRIPT_DRIVE%
-CD %_SCRIPT_PATH%
-SET ISO_DIR=iso
+SET menuISOFileList=%ROOT_DIR%/boot/.menuISOFileList.lst
+set menuISOChooseType=/boot/.menuISOChooseType.lst
 
 
-if not exist "%_SCRIPT_DRIVE%/boot" ( GOTO DoneISOContig )
+if not exist "%ROOT_DIR%/boot" ( GOTO DoneISOContig )
 if not exist "%_ContigEXETool%" ( GOTO SkipISOContig )
 echo ###################################################
 echo ######### Verifying that all ISO files ############
@@ -21,7 +22,7 @@ echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX>"%FragISOLogFile%"
 echo XXXXXXXXXX Fragmented files found: XXXXXXXXXX>>"%FragISOLogFile%"
 echo.>>"%FragISOLogFile%"
 reg ADD "HKCU\SOFTWARE\Sysinternals\Contig" /f /v EulaAccepted /d 1 >nul 2>&1
-"%_ContigEXETool%" -a "%ISO_DIR%\*.iso" | findstr /C:"is in" >>"%FragISOLogFile%"
+"%_ContigEXETool%" -a "%ISO_DIR%/*.iso" | findstr /C:"is in" >>"%FragISOLogFile%"
 reg DELETE "HKCU\SOFTWARE\Sysinternals\Contig" /f /v EulaAccepted >nul 2>&1
 echo.>>"%FragISOLogFile%"
 echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX>>"%FragISOLogFile%"
@@ -47,14 +48,13 @@ pause
 :DoneISOContig
 
 
-if not exist "%_SCRIPT_DRIVE%/boot" ( GOTO DonemenuISOFileList )
+if not exist "%ROOT_DIR%/boot" ( GOTO DonemenuISOFileList )
 if not exist "%menuISOFileList%" ( GOTO skipmenuISOFileList )
 echo ###################################################
 echo ################ Listing ISO files ################
 echo ###################################################
-set menuISOChooseType=/boot/.menuISOChooseType.lst
 echo.>%menuISOFileList%
-for /f "delims=" %%d in ('dir *.iso /b') do (
+for /f "delims=" %%d in ("dir %ISO_DIR%/*.iso /b") do (
 	echo.>>%menuISOFileList%
 	echo title Set ISO="%%d">>%menuISOFileList%
 	echo set MYISO=%%d>>%menuISOFileList%
---
+++ iso/verify-iso.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Ref for Bash Colors and formatting:
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
+
+ROOT_DIR=$(realpath $(dirname $0)/..)
+cd $ROOT_DIR
+
+function list_iso_files_to_menu() {
+  menuISOFileList=$ROOT_DIR/boot/.menuISOFileList.lst
+  menuISOChooseType=boot/.menuISOChooseType.lst
+  echo >$menuISOFileList
+  for isoFile in $1; do
+    echo "title Set ISO=\"$isoFile\"" >>$menuISOFileList
+    echo "set MYISO=$isoFile" >>$menuISOFileList
+    echo "configfile $menuISOChooseType" >>$menuISOFileList
+    echo >>$menuISOFileList
+  done
+}
+
+function verify_iso_files() {
+  S_B="\e[1m"
+  S_U="\e[4m"
+  S_BG_RED="\e[41m"
+  S_BG_GREEN="\e[42m"
+  S_FG_RED="\e[91m"
+  S_FG_GREEN="\e[92m"
+  S_FG_YELLOW="\e[93m"
+  S_FG_RESET="\e[39m"
+  S_RESET="\e[0m"
+  MARK_CHECK="$S_FG_GREEN✔$S_FG_RESET"
+  MARK_CROSS="$S_FG_RED✘$S_FG_RESET"
+  MARK_WARN="$S_FG_YELLOW⚠️ $S_FG_RESET"
+
+  countFragementedFiles=0
+  echo "Scanning ISO files…"
+  for isoFile in $1; do
+    read F N _ <<<"$(sudo filefrag $isoFile)"
+    # COLOR=$([ "1" == "$N" ] && echo $S_BOLD || echo $S_BOLD$S_FG_RED)
+    [ "1" == "$N" ] && {
+      COLOR=""
+      echo -e " $MARK_CHECK $COLOR$isoFile$S_RESET: contiguous ($N fragement found)"
+    } || {
+      COLOR="$S_B$S_BG_RED"
+      countFragementedFiles=$((countFragementedFiles + 1))
+      echo -e " $MARK_CROSS $COLOR$isoFile$S_RESET: non-contiguous ($N fragements found)"
+    }
+  done
+  [ "0" == $countFragementedFiles ] && echo "Done… $MARK_CHECK all checked files are contiguous!" || echo -e "Done… found 2 non-bootable ISO files.\n$MARK_WARN Please 'Delete+re-Copy' or 'Defrag' them!"
+}
+
+# scan recursively (regardless ISO_DIR = iso or .) and case-insensitively "*.iso" files
+isoFiles=$(find $1 -type f -iname "*.iso")
+
+verify_iso_files "$isoFiles"
+
+list_iso_files_to_menu "$isoFiles"
